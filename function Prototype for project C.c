ad1497066bc47b723742cc2ce5651016ca5edc3c
liberttefreiheit2019 <libertefreiheit2019@gmail.com>
2019-09-25 18:28:20 +1000

    Create function Prototype for project C.c
    welcome page and display menu

---
+++ function Prototype for project C.c
@@ -0,0 +1,60 @@
+//
+//  main.c
+//  function Prototype for project C
+//
+//  Created by Z C on 25/9/19.
+//  Copyright © 2019 Z C. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+
+         printf("Welcome to calendar reminder.\n");
+
+         return 0;
+
+};
+
+
+
+
+
+struct record{
+    int;
+    char;
+    float;
+    double;
+    
+};
+typedef struct record record_list;
+
+void display_manal(void){
+    
+    printf("1.\n");
+    printf("2.\n");
+    printf("3.\n");
+    printf("4.\n");
+    
+    
+}
+
+
+
+/*
+printf("%s",&record);
+
+
+
+
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
