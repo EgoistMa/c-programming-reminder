7640d106221dbb5b3b1ba80179c3cb425af4d360
nick <cn2nick@gmail.com>
2019-09-29 15:26:49 +1000

    Update group.c
    udpate add event

--- group.c
+++ group.c
@@ -1,41 +1,211 @@
 /*******************************************************************************
- * 48430 Fundamentals of C Programming - Assignment 3 Group
- * Name: Nuo Chen
- * Student ID: 12843773
- * Date of submission:25 Sep
- * A brief statement on what you could achieve (less than 50 words):
- * export database
- * read database
- * exit
+ * List header files 
  *******************************************************************************/
-#include<stdio.h>
+#include<stdio.h>/*printf,scanf*/
+#include<string.h>
+
+/*******************************************************************************
+ * List preprocessing directives - you may define your own.
+ *******************************************************************************/
+#define MAX_NUM_EVENTS 10
+#define MAX_COMMENT_LEN 400
 #define DB_EVENT "database"
 
+/*******************************************************************************
+ * List structs
+ *******************************************************************************/
 struct date_time
 {
+	int year;/*integer 4 numbers*/
     int month;/*integer between 1 and 12 (inclusive)*/
     int day;/*integer between 1 and 31 (inclusive)*/
-    int hour;/*integer between 0 and 23 (inclusive)*/
-    int minute;/*integer between 0 and 59 (inclusive)*/
+    int hour;/*integer between 1 and 23(inclusive)*/
 };
 typedef struct date_time date_time_t;
 
 struct event
 {
-	date_time_t time;
-	char comment[];
-
+	date_time_t remind_time;
+	char comment[MAX_COMMENT_LEN];
+	struct event *next;
 };
 typedef struct event event_t;
 
-void exportEvent();
-void readEvent();
+/*******************************************************************************
+ * Function prototypes 
+ *******************************************************************************/
+void print_menu (void);
+void addEvent(event_t *events, int *EVENT_NUM);
+void deleteEvent(event_t *events, int *EVENT_NUM);
+void exportEvent(event_t *events, int EVENT_NUM);
+void readEvent(event_t *events, int *EVENT_NUM);
 void exit();
 
+int DT_is_valid(const int year, const int month, 
+	const int day, const int hour);
+
+/*******************************************************************************
+ * Main
+ *******************************************************************************/
 int main(void)
 {
-
+	event_t events [MAX_NUM_EVENTS];
+    int EVENT_NUM=0;/*Cerent event number in the program*/
+    
+    do{
+        print_menu();
+        int choice;
+        scanf("%d", &choice);
+        while(getchar()!='\n');
+        switch(choice)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                exit(0);
+            default:printf("Invalid choice\n");
+        }
+    }while(1);
 	return 0;
+}
+
+void print_menu (void)
+{
+    printf("\n"
+           "1. \n"
+           "2. \n"
+           "3. \n"
+           "4. \n"
+           "5. \n"
+           "..."
+           "Enter choice (number between 1-.)>\n");
+}
+
+void addEvent(event_t *events, int *EVENT_NUM)
+{
+	if(*EVENT_NUM<MAX_NUM_EVENTS)
+    {
+        int year, month, day, hour;
+        while(1)
+        {
+        	printf("Enter year, month, day and hour seperated by spaces>\n");
+        	scanf("%d %d %d %d", &year, &month, &day, &hour);
+        	while(getchar()!='\n');
+        	if(DT_is_valid(year, month, day, hour))
+        	{
+        		break;
+        	}
+        	else
+        	{
+        		printf("Invalid Inputs\n");
+        	}
+        }
+        date_time_t date = {year, month, day, hour};
+        events[*EVENT_NUM].remind_time = date;
+
+        (*EVENT_NUM)++;
+    }
+    else
+    {
+        printf("Cannot add more events - memory full\n");
+        return;
+    }
+}
+
+int DT_is_valid(const int year, const int month, 
+	const int day, const int hour)
+	{
+		 if((year % 4 == 0 && year % 100 != 0)||(year % 400 == 0))/*leap year*/
+		 {
+		 	if(month >= 1 && month <= 12)
+		 	{
+		 		if(month == 1 || month == 3 || month == 5 ||
+		 			month == 7 || month == 8 || month == 10
+		 			|| month == 12)
+		 		{
+		 			if(day >= 1 && day <= 31)
+		 			{
+		 				if(hour >= 1 && hour <= 23)
+		 				{
+		 					return 1;
+		 				}
+		 			}
+		 		}
+		 		else if(month == 4 || month == 6 || month == 9
+		 			|| month == 11)
+		 		{
+		 			if(day >= 1 && day <= 30)
+		 			{
+		 				if(hour >= 1 && hour <= 23)
+		 				{
+		 					return 1;
+		 				}
+		 			}
+		 		}
+		 		else
+		 		{
+		 			if(day >= 1 && day <= 29)
+		 			{
+		 				if(hour >= 1 && hour <= 23)
+		 				{
+		 					return 1;
+		 				}
+		 			}
+		 		}
+		 	}
+		 }
+		 else
+		 {
+		 	if(month >= 1 && month <= 12)
+		 	{
+		 		if(month == 1 || month == 3 || month == 5 ||
+		 			month == 7 || month == 8 || month == 10
+		 			|| month == 12)
+		 		{
+		 			if(day >= 1 && day <= 31)
+		 			{
+		 				if(hour >= 1 && hour <= 23)
+		 				{
+		 					return 1;
+		 				}
+		 			}
+		 		}
+		 		else if(month == 4 || month == 6 || month == 9
+		 			|| month == 11)
+		 		{
+		 			if(day >= 1 && day <= 30)
+		 			{
+		 				if(hour >= 1 && hour <= 23)
+		 				{
+		 					return 1;
+		 				}
+		 			}
+		 		}
+		 		else
+		 		{
+		 			if(day >= 1 && day <= 28)
+		 			{
+		 				if(hour >= 1 && hour <= 23)
+		 				{
+		 					return 1;
+		 				}
+		 			}
+		 		}
+		 	}
+		 }
+
+		 return 0;
+	}
+
+void deleteEvent(event_t *events, int *EVENT_NUM)
+{
+
 }
 
 /*******************************************************************************
@@ -45,7 +215,7 @@ int main(void)
  * outputs:
  * - database file
  *******************************************************************************/
-void exportEvent()
+void exportEvent(event_t *events, int EVENT_NUM)
 {
 	FILE *fp =fopen(DB_EVENT, "w");
 
@@ -59,7 +229,7 @@ void exportEvent()
  * outputs:
  * - database file
  *******************************************************************************/
-void readEvent()
+void readEvent(event_t *events, int *EVENT_NUM)
 {
 	FILE *fp;
     if((fp = fopen(DB_EVENT, "r")) == NULL)
@@ -82,6 +252,6 @@ void readEvent()
  *******************************************************************************/
 void exit()
 {
-	exportEvent();
+	
 	exit(0);
 }
